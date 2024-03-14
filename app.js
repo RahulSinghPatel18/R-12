604777f475875ddcf678298714b5863236b9348a
Rahul Singh Patel <rahulsinghpatel198@gmail.com>
2024-03-14 17:31:48 +0530

    mnkn

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ const app = express();
 
 
 app.listen(process.env.PORT, () =>{
-    console.log(`The Server is running on port ${process.env.PORT   }`);
+    console.log(`The Server is running on port 
+    ${process.env.PORT   }`);
 });
